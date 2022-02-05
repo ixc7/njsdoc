d62a22ac224ab1c98a9fe449fb29686b9befd93b
ixc7 <isaacxpreston7@gmail.com>
2022-02-05 09:59:54 -0500

    grep code blocks from document

--- index.js
+++ index.js
@@ -2,33 +2,25 @@
 
 import { readFileSync } from 'fs'
 
+const filepath = new URL('docs/fs.md', import.meta.url).pathname
+const doc = readFileSync(filepath).toString().split('```')
 
-const filepath = new URL('docs/os.md', import.meta.url).pathname
-
-const doc = readFileSync(filepath).toString()
-
-const codeSplit = doc.split('```')
-const len = Math.floor(codeSplit.length / 2)
-
-// get odd indexes (1, 3, 5...)
-let cur = 1
+let index = 1
 let num = 1
+let result = ''
 
+const printCodeBlocks = (result = '') => {
+  if (!doc[index]) return result
 
-const printCodeBlocks = (interval = 750) => {
-  if (!codeSplit[cur]) {
-    console.log('done')
-    process.exit(0)
-  }
-  const header = `// example ${num}/${len}\n\n`
-  const formattedCode = header + codeSplit[cur].split('\n').slice(1).join('\n')
-  console.log(formattedCode)
+  result += 
+    `\n\n// block \x1b[1m${num}\x1b[0m\n\n` +
+    doc[index].split('\n').slice(1).join('\n')
 
-  setTimeout(() => {
-    cur += 2
-    num += 1
-    printCodeBlocks()
-  }, interval)
+  num += 1
+  index += 2
+  
+  return printCodeBlocks(result)
 }
 
-printCodeBlocks(250)
+console.clear()
+console.log(printCodeBlocks())
