d2a27e547b985ab07d171e70c9eab7f952c3bd9d
ixc7 <isaacxpreston7@gmail.com>
2022-02-07 09:41:08 -0500

    strip YAML 1

--- index.js
+++ index.js
@@ -2,19 +2,24 @@
 
 import { readFileSync } from 'fs'
 
-const filepath = new URL('docs/os.md', import.meta.url).pathname
+const filepath = new URL('docs/buffer.md', import.meta.url).pathname
 const initial = readFileSync(filepath).toString()
 
+// split into text blocks and code blocks
 const printBlocks = opts => {
-  const { index, num, result, source, title } = opts
+  const { index, num, result, source, title, verbose } = opts
   const localSource = [ ...opts.source ]
   
   if (!source[index]) return result
-  
-  result[num] = 
+
+  const parsed = localSource[index].split('\n').slice(1).join('\n')
+  result.push(parsed)
+    
+  if (verbose) console.log(
     `\n\x1b[1m\x1b[38;5;161m<${title}-${num}>\x1b[0m\n\n` + 
-    localSource[index].split('\n').slice(1).join('\n') + 
+    parsed + 
     `\n\x1b[38;5;52m</${title}-${num}>\x1b[0m\n\n`
+  )
 
   return printBlocks({
     ...opts,
@@ -24,37 +29,32 @@ const printBlocks = opts => {
   })
 }
 
-console.log('\x1Bc\x1b[3J')
-const codeBlocks = printBlocks({ source: initial.split('```'), index: 1, num: 0, result: {}, title: 'Code' })
-const Blocks = printBlocks({ source: initial.split('```'), index: 0, num: 0, result: {}, title: 'Text'})
+const codeBlocks = printBlocks({ source: initial.split('```'), index: 1, num: 0, result: [], title: 'Code' })
+const textBlocks = printBlocks({ source: initial.split('```'), index: 0, num: 0, result: [], title: 'Text'})
 
-console.log(
-'\n\x1b[1m\x1b[38;5;35mTEXT BLOCKS :\x1b[0m\n\n',
-Blocks[0], Blocks[1], Blocks[2],
+// get rid of YAML comments
+const yamlStripped = []
 
-'\n\x1b[1m\x1b[38;5;35mCODE BLOCKS :\x1b[0m\n\n',
-codeBlocks[0], codeBlocks[1], codeBlocks[2]
-)
+// TODO check + recurse if multiple instances
+for (let i = 0; i < textBlocks.length; i += 1) {
+    const text = textBlocks[i]
+    const start = textBlocks[i].indexOf('<!-- YAML')
+    const end = textBlocks[i].indexOf('-->')
+    if (start < 0 || end < 0) yamlStripped.push(text)
+    else {
+      const firstHalf = text.slice(0, start)
+      const lastHalf = text.slice(end + '-->'.length)
+      yamlStripped.push(firstHalf + lastHalf)
+    }
+}
 
-
-/*
-
-'## Promises API\n' +
-    '\n' +
-    '<!-- YAML\n' +
-    'added: v10.0.0\n' +
-    'changes:\n' +
-    '  - version: v14.0.0\n' +
-    '    pr-url: https://github.com/nodejs/node/pull/31553\n' +
-    "    description: Exposed as `require('fs/promises')`.\n" +
-    '  - version:\n' +
-    '    - v11.14.0\n' +
-    '    - v10.17.0\n' +
-    '    pr-url: https://github.com/nodejs/node/pull/26581\n' +
-    '    description: This API is no longer experimental.\n' +
-    '  - version: v10.1.0\n' +
-    '    pr-url: https://github.com/nodejs/node/pull/20504\n' +
-    "    description: The API is accessible via `require('fs').promises` only.\n" +
-    '-->\n' +
-
-*/
+// log results
+// console.log('\x1Bc\x1b[3J')
+for (let i = 0; i < textBlocks.length - 1; i += 1) {
+  console.log(
+    `\n\n-------\n\x1b[38;5;52minitial (${i})\x1b[0m\n-------\n\n`,
+    textBlocks[i],
+    `\n\n-----------\n\x1b[1m\x1b[38;5;161mno yaml (${i})\x1b[0m\n-----------\n\n`,
+    yamlStripped[i]
+  )
+}
